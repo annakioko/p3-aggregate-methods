53ec371a2b0ac9fdfb8de99c6a763d63d120b5e2
Anna Kioko <annakioko00@gmail.com>
2024-03-03 17:27:31 +0300

    completed assignment

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -11,6 +11,10 @@ class Student:
             course.add_enrollment(enrollment)
         else:
             raise TypeError("course must be an instance of Course")
+        
+    def course_count(self):
+        return len(self._enrollments)
+
 
     def get_enrollments(self):
         return self._enrollments.copy()
@@ -45,3 +49,18 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+    
+
+@classmethod
+def aggregate_enrollments_per_day(cls):
+      enrollment_count = {}
+      for e in cls.all:
+       date = e.get_enrollment_date().date()
+       enrollment_count[date] = enrollment_count.get(date, 0) + 1
+       return enrollment_count
+
+def aggregate_average_grade(self):
+    total_grades = sum(self._grades.values())
+    num_courses = len(self._grades)
+    average_grade = total_grades / num_courses
+    return average_grade
